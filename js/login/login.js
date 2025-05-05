980450c6451242d04b2ec51d32e93ecaa4b57717
Gabs-Attuy <gabrielattuy1010@gmail.com>
2025-05-05 20:38:11 -0300

    Feat: Login

---
+++ js/login/login.js
@@ -0,0 +1,29 @@
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    try {
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: usuario, password: senha })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Login bem-sucedido:', data);
+            localStorage.setItem('userData', JSON.stringify(data));
+            alert('Login realizado com sucesso!');
+            window.location.href = '';
+        } else {
+            alert('Erro ao realizar login. Verifique suas credenciais.');
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+        alert('Ocorreu um erro. Tente novamente mais tarde.');
+    }
+});
